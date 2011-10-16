e6c9479023abd0b55bc86203bbb7c11ec2e9280c
Jonas Folleso <jonas@follesoe.no>
2011-10-16 22:36:22 +0200

    Changed metadata.

--- VSMonoTouch/Properties/AssemblyInfo.cs
+++ VSMonoTouch/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: InternalsVisibleTo("VSMonoTouch_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100db5f6823765cc0097d3f29e2657fee0d71b1ae26a2c6f114d719dd062d9052f64e6951f8f131fb019bb2463f9d1ea48855fd16e52cce9382161693df2666f895c07ee60bc5c14d3cbedfd677551bf7df715cce0489fe599d68bb303d1b4e07a4aa985511ebbc8c9de80240252184b1dc0a1fcf3c63883da29beceef5f0bb8e8e")]
 [assembly: InternalsVisibleTo("VSMonoTouch_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100db5f6823765cc0097d3f29e2657fee0d71b1ae26a2c6f114d719dd062d9052f64e6951f8f131fb019bb2463f9d1ea48855fd16e52cce9382161693df2666f895c07ee60bc5c14d3cbedfd677551bf7df715cce0489fe599d68bb303d1b4e07a4aa985511ebbc8c9de80240252184b1dc0a1fcf3c63883da29beceef5f0bb8e8e")]
